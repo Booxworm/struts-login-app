34fc370012d9b0bc9e5e187c06090dcad6cd99d5
Wonn Jen <lee.wonnjen@gmail.com>
2020-11-10 17:59:49 +0800

    Implement full DAO pattern

--- src/main/java/Login.java
+++ src/main/java/Login.java
@@ -1,40 +1,18 @@
 package com.grp61.app;
 
-import java.util.Map;
-import org.apache.struts2.dispatcher.SessionMap;
-import org.apache.struts2.interceptor.SessionAware;
-
-public class Login implements SessionAware{
+public class Login {
     private String username, password;
-    SessionMap<String,String> sessionmap;
-    
-    public String getUsername() {
+
+    public Login(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername(){
         return username;
     }
     
-    public void setUsername(String username) {
-        this.username = username;
-    }
-    
-    public String getPassword() {
+    public String getPassword(){
         return password;
-    }
-    
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    
-    public String execute(){
-        if(LoginDao.validate(username, password)){
-            return "success";
-        }
-        else{
-            return "error";
-        }
-    }
-    
-    public void setSession(Map map) {
-        sessionmap=(SessionMap)map;
-        sessionmap.put("login","true");
     }
 }
--- src/main/java/LoginDao.java
+++ src/main/java/LoginDao.java
@@ -1,38 +1,5 @@
 package com.grp61.app;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
   
-public class LoginDao {
-    public static boolean validate(String username, String password){
-        boolean status=false;
-
-        try{
-            // if (username.equals("admin") && password.equals("admin")){
-            //     status = true;
-            // }
-
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/cz3002_login?user=admin&password=admin&serverTimezone=UTC"
-            );
-
-            PreparedStatement ps = con.prepareStatement(
-                "SELECT * FROM login_info " +
-                "WHERE username=? AND password=?"
-            );
-            ps.setString(1,username);
-            ps.setString(2,password);
-
-            ResultSet rs = ps.executeQuery();  
-            status = rs.next();
-            con.close();
-
-        } catch(Exception e){
-            e.printStackTrace();
-        }
-        return status;
-    }
+public interface LoginDao{
+    public boolean validate();
 }
